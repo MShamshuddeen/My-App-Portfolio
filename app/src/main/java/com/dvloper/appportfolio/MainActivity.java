bcef25a8510f013097ad6dc497c727ecc07daaff
mshamshuddeen <shamshuddeen@hotmail.com>
2016-12-06 18:55:39 +0530

    adding button object

--- app/src/main/java/com/dvloper/appportfolio/MainActivity.java
+++ app/src/main/java/com/dvloper/appportfolio/MainActivity.java
@@ -2,12 +2,27 @@ package com.dvloper.appportfolio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+     Button btn1,btn2,btn3,btn4,btn5,btn6;
+     Toast toast;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn1 = (Button)findViewById(R.id.button);
+        btn2 = (Button)findViewById(R.id.button2);
+        btn3 = (Button)findViewById(R.id.button3);
+        btn4 = (Button)findViewById(R.id.button4);
+        btn5 = (Button)findViewById(R.id.button5);
+        btn6 = (Button)findViewById(R.id.button6);
+
+
+    }
+    public void showToast(View view){
+
     }
 }
