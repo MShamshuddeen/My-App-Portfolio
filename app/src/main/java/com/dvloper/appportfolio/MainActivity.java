b704e5ccdbbaec7abef0a90c55fe907d910d57af
mshamshuddeen <shamshuddeen@hotmail.com>
2016-12-06 19:18:46 +0530

    onclick toast message added

--- app/src/main/java/com/dvloper/appportfolio/MainActivity.java
+++ app/src/main/java/com/dvloper/appportfolio/MainActivity.java
@@ -7,22 +7,19 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-     Button btn1,btn2,btn3,btn4,btn5,btn6;
-     Toast toast;
+    Toast toast;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn1 = (Button)findViewById(R.id.button);
-        btn2 = (Button)findViewById(R.id.button2);
-        btn3 = (Button)findViewById(R.id.button3);
-        btn4 = (Button)findViewById(R.id.button4);
-        btn5 = (Button)findViewById(R.id.button5);
-        btn6 = (Button)findViewById(R.id.button6);
 
 
     }
     public void showToast(View view){
+        Button b = (Button)view;
+        String buttontext = b.getText().toString();
+        toast.makeText(this,"This button will launch "+buttontext+" Project",Toast.LENGTH_SHORT).show();
+
 
     }
 }
